224769e3afa090f3dbf6d43bf2362acb0bca4627
Ravi-Teja-S <ravithejatheja03@gmail.com>
2025-04-07 21:15:07 +0530

    deleted .env

--- script.js
+++ script.js
@@ -11,27 +11,5 @@ function navigateToHomePage() {
     secondPage.classList.remove("visible");
     document.getElementById("home-page").classList.remove("hidden");
 }
-// Select all dropdown buttons
-// Toggle dropdown on click
-document.querySelectorAll('.dropbtn').forEach(button => {
-    button.addEventListener('click', (e) => {
-        e.stopPropagation(); // Prevent window click from closing it immediately
-        const dropdownContent = button.nextElementSibling;
-        dropdownContent.classList.toggle('show');
 
-        // Close other dropdowns
-        document.querySelectorAll('.dropdown-content').forEach(content => {
-            if (content !== dropdownContent) {
-                content.classList.remove('show');
-            }
-        });
-    });
-});
-
-// Close dropdown if clicked outside
-window.addEventListener('click', () => {
-    document.querySelectorAll('.dropdown-content').forEach(content => {
-        content.classList.remove('show');
-    });
-});
 
